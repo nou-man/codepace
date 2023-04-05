808bd2255359bae5101a9401f142ac1e2d38a67e
nou-man <4pa20cs059@pace.edu.in>
2023-04-05 14:31:02 +0530

    added:navbar&routing&Concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,21 @@ import React, { Fragment } from 'react';
 import './App.css';
 import Card from './components/Card'
 import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom"
+import About from './components/About';
 
 function App() {
 
   return (
-    <Fragment>
+    <>
       <Navbar />
-      <Card />
-    </Fragment>
+      {/* <Card /> */}
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </>
   );
 }
 
